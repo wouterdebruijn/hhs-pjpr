eab405bedeb21622eff257f2a97bc9c37f602bdb
Wouter de Bruijn <wouter@hedium.nl>
2023-05-13 14:13:40 +0200

    🎨 Changed access token to full SAS variable

--- AMQP/receive.py
+++ AMQP/receive.py
@@ -1,40 +1,14 @@
 import uamqp
-import urllib
-import uuid
+import sys
 
-from base64 import b64encode, b64decode
-from hashlib import sha256
-from time import time
 from urllib import parse
-from hmac import HMAC
 
-def generate_sas_token(uri, key, policy_name, expiry=3600):
-    ttl = time() + expiry
-    sign_key = "%s\n%d" % ((parse.quote_plus(uri)), int(ttl))
-    print(sign_key)
-    signature = b64encode(HMAC(b64decode(key), sign_key.encode('utf-8'), sha256).digest())
-
-    rawtoken = {
-        'sr' :  uri,
-        'sig': signature,
-        'se' : str(int(ttl))
-    }
-
-    if policy_name is not None:
-        rawtoken['skn'] = policy_name
-
-    return 'SharedAccessSignature ' + parse.urlencode(rawtoken)
-
-iot_hub_name = 'pjpr-iot-hub'
+iot_hub_name = 'pjpr-iothub'
 hostname = '{iot_hub_name}.azure-devices.net'.format(iot_hub_name=iot_hub_name)
-device_id = 'pjpr-amqp-device'
-access_key = 'M2EvmHkiYSXlylI8qFB4hjvb4xCBSCIIWFQt9j3Zp/s='
-username = '{device_id}@sas.{iot_hub_name}'.format(
-    device_id=device_id, iot_hub_name=iot_hub_name)
-sas_token = generate_sas_token('{hostname}/devices/{device_id}'.format(
-    hostname=hostname, device_id=device_id), access_key, None)
+device_id = 'pjpr-device-1'
+username = '{device_id}@sas.{iot_hub_name}'.format(device_id=device_id, iot_hub_name=iot_hub_name)
+sas_token = sys.argv[1]
 
-# ...
 # Create a send client for the device-to-cloud send link on the device
 operation = '/devices/{device_id}/messages/devicebound'.format(device_id=device_id)
 uri = 'amqps://{}:{}@{}{}'.format(parse.quote_plus(username), parse.quote_plus(sas_token), hostname, operation)
