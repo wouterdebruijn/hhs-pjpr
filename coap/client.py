f43d967b7150f1b451fb6d5ee2492fb38d26ac39
Wouter de Bruijn <wouter@hedium.nl>
2023-06-04 16:44:15 +0200

    🧪 Verified PoC

---
+++ coap/client.py
@@ -0,0 +1,27 @@
+import logging
+import asyncio
+import time
+
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    print("Preforming CoAP Test")
+    protocol = await Context.create_client_context()
+
+    start = time.time()
+    request = Message(code=GET, uri='coap://localhost/time')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print(f'TimeLocal: {(time.time() - start) * 1000} ms')
+        print(f'TimeServer: {(float(response.payload) - start) * 1000} ms')
+        
+
+if __name__ == "__main__":
+    asyncio.run(main())
