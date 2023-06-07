38a43af60bbf35df59cfbaac2eaa7f041b80c142
jugrajsingh01 <58332564+jugrajsingh01@users.noreply.github.com>
2023-06-07 11:29:40 +0200

    dtls credentials

--- coap/client.py
+++ coap/client.py
@@ -9,9 +9,10 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     print("Preforming CoAP Test")
     protocol = await Context.create_client_context()
+    protocol.client_credentials.load_from_dict({"coaps://13.80.102.200/time": {"dtls": {"psk": {"ascii": "11223344"}, "client-identity": {"ascii": "client_Identity"}}}})
 
     start = time.time()
-    request = Message(code=GET, uri='coap://localhost/time')
+    request = Message(code=GET, uri='coaps://13.80.102.200/time')
 
     try:
         response = await protocol.request(request).response
@@ -19,9 +20,10 @@ async def main():
         print('Failed to fetch resource:')
         print(e)
     else:
-        print(f'TimeLocal: {(time.time() - start) * 1000} ms')
-        print(f'TimeServer: {(float(response.payload) - start) * 1000} ms')
-        
+        # print(f'TimeLocal: {(time.time() - start) * 1000} ms')
+        # print(f'TimeServer: {(float(response.payload) - start) * 1000} ms')
+        print(response.payload)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
